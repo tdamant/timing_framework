0b40425063237d7a1b6ea8343f4b23219ec42c25
Tom Damant <tom@gluon.local>
2019-02-25 15:44:26 +0000

    run tests adds result objects to result array

--- __tests__/timing.test.js
+++ __tests__/timing.test.js
@@ -1,56 +1,54 @@
 const timing = require('../src/timing')
+let arr = timing.createArrays(5000, 10000)
 describe("creating arrays", () => {
   test("can array wich starts with specificed array size", () => {
-    arr = timing.createArrays(500, 100000)
-    expect(arr[0].length).toBe(500)
+    expect(arr[0].length).toBe(5000)
   });
   test("can create array of arrays", () => {
-    arr = timing.createArrays(500, 100000)
     expect(arr instanceof Array).toBe(true);
     expect(arr[0] instanceof Array).toBe(true)
   });
   test("arrays go from 0 to specified length - 1", () => {
-    arr = timing.createArrays(5000, 100000)
     expect(arr[0][0]).toBe(0);
     firstArr = arr[0]
     expect(firstArr[firstArr.length - 1]).toBe(4999)
-  })
-  test("can array with specified start size", () => {
-    arr = timing.createArrays(500, 100000)
-    expect(arr[0].length).toBe(500)
   });
   test("can create array with specified end size", () => {
-    arr = timing.createArrays(5000, 100000)
-    expect(arr[arr.length - 1].length).toBe(100000)
+    expect(arr[arr.length - 1].length).toBe(10000)
   });
   test("create array with specified step size", () => {
-    arr = timing.createArrays(500, 10000, 10)
-    expect(arr[1].length - arr[0].length).toBe(10)
+    arr1 = timing.createArrays(500, 10000, 10)
+    expect(arr1[1].length - arr1[0].length).toBe(10)
   });
 });
 
   describe("running tests", () =>{
+
     test("function to test called on each array", () =>{
       const shuffle = jest.fn();
-      arr = timing.createArrays(5000, 10000)
       timing.runTests(arr, shuffle)
       expect(shuffle).toHaveBeenCalledTimes(arr.length)
     });
     test("return an array of objects with label and data params", () =>{
-      arr = timing.createArrays(5000, 10000)
       reverse = (arr) => {
         arr.reverse()
       };
       result = timing.runTests(arr, reverse);
-      expect(result instanceof Array).toBe(true);
+      console.log(result);
+      expect(result.length).toBe(2)
     });
     test("it creates array with objects containing labels", () => {
-      arr = timing.createArrays(5000, 10000)
       reverse = (arr) => {
         arr.reverse()
       };
       result = timing.runTests(arr, reverse);
-      console.log(result)
       expect(result[0].label).toBe(5000)
+    });
+    test("it creates array with objects containing data params", () => {
+      reverse = (arr) => {
+        arr.reverse()
+      };
+      result = timing.runTests(arr, reverse);
+      expect(result[0].data).not.toBe(null)
     });
   });
